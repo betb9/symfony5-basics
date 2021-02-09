f170c1eae4c166bbe0c8eeb46212ff1d79463a74
Etienne <bender.etienne57@gmail.com>
2021-02-09 23:40:16 +0100

    Make Routes with Symfony Annotations instead of using routes.yaml file

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,9 +5,14 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController {
 
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homepage() {
         return new Response('What a bewitching controller we have conjured!');
     }
