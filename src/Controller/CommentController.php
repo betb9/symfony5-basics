fc4fa8a4fa77f43f2d004834adef5d52e9da3984
Etienne <bender.etienne57@gmail.com>
2021-02-11 21:59:19 +0100

    Create function that returns new vote count for a comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,9 +5,25 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController {
 
+    /**
+     * @Route("/comments/{id}/vote/{direction}")
+     * @param $id
+     * @param $direction
+     * @return JsonResponse
+     */
+    public function commentVote($id, $direction): JsonResponse {
+        if($direction === 'up') {
+            $currentVote = rand(7, 100);
+        } else {
+            $currentVote = rand(0, 5);
+        }
 
+        return $this->json(['votes' => $currentVote]);
+    }
 
 }
