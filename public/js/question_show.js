42a72c234501da7c4a819d29dbc1b2cb51b93619
Etienne <bender.etienne57@gmail.com>
2021-02-11 22:19:06 +0100

    Add some JavaScript to call the API and get the vote

---
+++ public/js/question_show.js
@@ -0,0 +1,15 @@
+/**
+ * Simple (ugly) code to handle the comment vote/down
+ */
+let $container = $('.js-vote-arrows');
+$container.find('a').on('click', (e) => {
+   e.preventDefault();
+   let $link = $(e.currentTarget);
+
+   $.ajax({
+       url: '/comments/10/vote/' + $link.data('direction'),
+       method: 'POST'
+   }).then(data => {
+       $container.find('.js-vote-total').text(data.votes);
+    })
+});
