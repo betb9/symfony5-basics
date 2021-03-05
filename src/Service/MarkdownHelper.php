8df8968b656d00ba2033bce13e351ef7215c0a88
Etienne <bender.etienne57@gmail.com>
2021-03-05 18:35:38 +0100

    Add a debug parameter to MarkdownHelper constructor and bind it in service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -10,15 +10,18 @@ class MarkdownHelper {
 
 	private $markdownParser;
 	private $cache;
+	private $isDebug;
 
 	/**
 	 * MarkdownHelper constructor.
 	 * @param MarkdownParserInterface $markdownParser
 	 * @param CacheInterface $cache
+	 * @param bool $isDebug
 	 */
-	public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache) {
+	public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug) {
 		$this->markdownParser = $markdownParser;
 		$this->cache = $cache;
+		$this->isDebug = $isDebug;
 	}
 
 	/**
@@ -27,6 +30,9 @@ class MarkdownHelper {
 	 * @throws InvalidArgumentException
 	 */
 	public function parse(string $source): string {
+		if(!$this->isDebug) {
+			return $this->markdownParser->transformMarkdown($source);
+		}
     return $this->cache->get('markdown_'.md5($source), function() use($source) {
       return $this->markdownParser->transformMarkdown($source);
     });
