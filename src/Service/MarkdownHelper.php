896723b6a824c6b0c0a082225ad6c60874a57179
Etienne <bender.etienne57@gmail.com>
2021-03-06 23:59:34 +0100

    Working with service autowiring config

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,24 +4,40 @@ namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Psr\Cache\InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper {
 
+	/**
+	 * @var MarkdownParserInterface
+	 */
 	private $markdownParser;
+	/**
+	 * @var CacheInterface
+	 */
 	private $cache;
+	/**
+	 * @var bool
+	 */
 	private $isDebug;
+	/**
+	 * @var LoggerInterface
+	 */
+	private $mdLogger;
 
 	/**
 	 * MarkdownHelper constructor.
 	 * @param MarkdownParserInterface $markdownParser
 	 * @param CacheInterface $cache
 	 * @param bool $isDebug
+	 * @param LoggerInterface $mdLogger
 	 */
-	public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug) {
+	public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $mdLogger) {
 		$this->markdownParser = $markdownParser;
 		$this->cache = $cache;
 		$this->isDebug = $isDebug;
+		$this->mdLogger = $mdLogger;
 	}
 
 	/**
@@ -30,6 +46,10 @@ class MarkdownHelper {
 	 * @throws InvalidArgumentException
 	 */
 	public function parse(string $source): string {
+		if(stripos($source, 'cat') !== false) {
+			$this->mdLogger->info('Meow!');
+		}
+
 		if(!$this->isDebug) {
 			return $this->markdownParser->transformMarkdown($source);
 		}
