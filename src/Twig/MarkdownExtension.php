b713b28d631c0f7d8e3709b1e6e231ba6cba1b21
Etienne <bender.etienne57@gmail.com>
2021-03-07 01:13:56 +0100

    Make the markdown twig extension works

--- src/Twig/MarkdownExtension.php
+++ src/Twig/MarkdownExtension.php
@@ -2,31 +2,37 @@
 
 namespace App\Twig;
 
+use App\Service\MarkdownHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class MarkdownExtension extends AbstractExtension
 {
-    public function getFilters(): array
-    {
+	/**
+	 * @var MarkdownHelper
+	 */
+	private $markdownHelper;
+
+
+	/**
+	 * MarkdownExtension constructor.
+	 * @param MarkdownHelper $markdownHelper
+	 */
+	public function __construct(MarkdownHelper $markdownHelper) {
+		$this->markdownHelper = $markdownHelper;
+	}
+
+	public function getFilters(): array {
         return [
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
+            new TwigFilter('parse_markdown', [$this, 'parseMarkdown'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('function_name', [$this, 'doSomething']),
-        ];
-    }
-
-    public function doSomething($value)
-    {
-        // ...
+    public function parseMarkdown($value) {
+        return $this->markdownHelper->parse($value);
     }
 }
