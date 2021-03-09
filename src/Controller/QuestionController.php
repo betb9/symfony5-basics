ee52679fc78d4b89b19b53f794d85776b9df5f3f
Etienne <bender.etienne57@gmail.com>
2021-03-09 10:36:02 +0100

    Use bundle and Question service parameter to automatically find question by slug

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -60,24 +60,13 @@ class QuestionController extends AbstractController
 
 	/**
 	 * @Route("/questions/{slug}", name="app_question_show")
-	 * @param $slug
-	 * @param MarkdownHelper $markdownHelper
-	 * @param EntityManagerInterface $entityManager
+	 * @param Question $question
 	 * @return Response
 	 */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question): Response
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
-        }
-
-        $repository = $entityManager->getRepository(Question::class);
-	    /**
-	     * @var Question|null $question
-	     */
-        $question = $repository->findOneBy(['slug' => $slug]);
-        if(!$question) {
-        	throw $this->createNotFoundException(sprintf('no question found!'));
         }
 
         $answers = [
